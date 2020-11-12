5e18a3985320eb2a99284e6f8a80fa05b492ba8d
Armando <rojas.armani@gmail.com>
2020-11-12 12:28:21 -0500

    commit tree 4c200a162de8f110c72e84c0cb2d50bc9d2fec7e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(array)
+    if block_given?
+        i=0
+        mod_ary = []
 
+        while i < array.length
+            new_val = yield (array[i]) 
+            mod_ary << new_val
+            i = i + 1
+        end
+        mod_ary
+    else
+        "No Block Given!"
+    end
+end
